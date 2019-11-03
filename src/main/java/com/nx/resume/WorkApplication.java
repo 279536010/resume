f486e5f55ff80fd141f6350cce65820c3ae838ed
njm <279536010@qq.com>
2019-11-03 21:19:55 +0800

    初始化项目

---
+++ src/main/java/com/nx/resume/WorkApplication.java
@@ -0,0 +1,13 @@
+package com.nx.resume.worke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nx/resume/worke/WorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nx.resume.worke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
