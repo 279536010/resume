dd3aba52e03ce19ceacb9869c4fe05ab88b840a9
xuwei <1061714188@qq.com>
2019-11-04 22:44:12 +0800

    service register

--- src/main/java/com/nx/resume/WorkApplication.java
+++ src/main/java/com/nx/resume/WorkApplication.java
@@ -1,13 +1,26 @@
-package com.nx.resume.worke;
+package com.nx.resume;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WorkApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WorkApplication.class, args);
+    }
+
+    @RestController
+    class EchoController {
+        @GetMapping(value = "/echo/{string}")
+        public String echo(@PathVariable String string) {
+            return string;
+        }
     }
 
 }
